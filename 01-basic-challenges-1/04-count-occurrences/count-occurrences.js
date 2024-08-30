a26575a2c6bc571ca9779112fd3e79bf6de7846f
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-08-30 18:27:51 +0200

    Solved count occurences challenge

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    let count = 0;
+
+    for (const letter of str) {
+        if (letter == char) {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 module.exports = countOccurrences;
