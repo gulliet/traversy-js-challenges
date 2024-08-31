c8512845b327b2cec3e219197c386db93346cc3f
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-08-31 09:39:21 +0200

    Max challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    let max = 0;
+
+    arr.forEach((num) => {
+        if (num > max) max = num;
+    });
+
+    return max;
+}
 
 module.exports = findMaxNumber;
