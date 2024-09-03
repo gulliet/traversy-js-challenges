0c38d2db9b689316151d46293ea565f2ef32fa9d
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-09-03 09:42:19 +0200

    Solved title case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,21 @@
-function titleCase() {}
+function titleCase(str) {
+    let withinAWord = false;
+    const arr = str.split("");
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === " ") {
+            withinAWord = false;
+            continue;
+        }
+        if (withinAWord) {
+            arr[i] = arr[i].toLowerCase();
+        } else {
+            arr[i] = arr[i].toUpperCase();
+            withinAWord = true;
+            console.log(`${i} ${arr[i]}`);
+        }
+    }
+
+    return arr.join("");
+}
 
 module.exports = titleCase;
